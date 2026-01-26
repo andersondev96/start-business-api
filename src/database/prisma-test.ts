07c831a60f8cd4ef0c6d414c1709a21212e6354e
Anderson Fernandes <anderson.fernandes96@outlook.com>
2026-01-26 17:41:20 -0300

    fix: Adjusting prisma version

--- src/database/prisma-test.ts
+++ src/database/prisma-test.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from '@prisma/client'
+import path from 'path'
+
+const dbPath = path.resolve(__dirname, '../../test/prisma/test.db')
 
 export const prismaTest = new PrismaClient({
   datasources: {
     db: {
-      url: 'file:./test.db',
+      url: `file:${dbPath}`,
     },
   },
 })
