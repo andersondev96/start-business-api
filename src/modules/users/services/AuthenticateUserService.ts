830d55f0c87ea109ad70df28b4610107b66c0a55
Anderson Fernandes <anderson.fernandes96@outlook.com>
2026-02-07 19:36:29 -0300

    refactor: Modified AuthenticateUserService

--- src/modules/users/services/AuthenticateUserService.ts
+++ src/modules/users/services/AuthenticateUserService.ts
@@ -1,96 +1,83 @@
-import { sign } from "jsonwebtoken";
-import { inject, injectable } from "tsyringe";
+import { inject, injectable } from 'tsyringe'
 
-import authConfig from "@config/auth";
-import { AppError } from "@shared/errors/AppError";
-
-import { IDateProvider } from "@shared/container/providers/DateProvider/models/IDateProvider";
-import { getUserAvatarUrl } from "@shared/utils/getFilesUrl";
-import { IHashProvider } from "../providers/HashProvider/models/IHashProvider";
-import { IUsersRepository } from "../repositories/IUsersRepository";
-import { IUsersTokenRepository } from "../repositories/IUsersTokenRepository";
+import authConfig from '@config/auth'
+import { AppError } from '@shared/errors/AppError'
+import { IDateProvider } from '@shared/container/providers/DateProvider/models/IDateProvider'
+import { FileUrlBuilder } from '@shared/utils/fileUrlBuilder'
+import { IHashProvider } from '../providers/HashProvider/models/IHashProvider'
+import { IUsersRepository } from '../repositories/IUsersRepository'
+import { IUsersTokenRepository } from '../repositories/IUsersTokenRepository'
+import type { ITokenProvider } from '@shared/container/providers/TokenProvider/models/ITokenProvider'
 
 interface IRequest {
-  email: string;
-  password: string;
+  email: string
+  password: string
 }
 
 interface IResponse {
   user: {
-    name: string;
-    email: string;
-    avatar: string;
-  };
-  token: string;
-  refresh_token: string;
+    name: string
+    email: string
+    avatar: string | null
+  }
+  token: string
+  refresh_token: string
 }
 
 @injectable()
 export class AuthenticateUserService {
   constructor(
-    @inject("UsersRepository")
+    @inject('UsersRepository')
     private usersRepository: IUsersRepository,
-    @inject("UsersTokenRepository")
+    @inject('UsersTokenRepository')
     private usersTokenRepository: IUsersTokenRepository,
-    @inject("HashProvider")
+    @inject('HashProvider')
     private hashProvider: IHashProvider,
-    @inject("DayjsDateProvider")
-    private dateProvider: IDateProvider
-  ) { }
+    @inject('DayjsDateProvider')
+    private dateProvider: IDateProvider,
+    @inject('TokenProvider')
+    private tokenProvider: ITokenProvider
+  ) {}
   public async execute({ email, password }: IRequest): Promise<IResponse> {
-    const user = await this.usersRepository.findByMail(email);
+    const user = await this.usersRepository.findByMail(email)
 
     if (!user) {
-      throw new AppError("Incorrect email/password combination", 401);
+      throw new AppError('Incorrect email/password combination', 401)
     }
 
     const passwordMatched = await this.hashProvider.compareHash(
       password,
       user.password
-    );
+    )
 
     if (!passwordMatched) {
-      throw new AppError("Incorrect email/password combination", 401);
+      throw new AppError('Incorrect email/password combination', 401)
     }
 
-    const {
-      secret_token,
-      expires_in_token,
-      secret_refresh_token,
-      expires_in_refresh_token,
-      expires_refresh_token_days
-    } = authConfig;
-
-    const token = sign({}, secret_token, {
-      subject: user.id,
-      expiresIn: expires_in_token
-    });
-
-    const refresh_token = sign({ email }, secret_refresh_token, {
-      subject: user.id,
-      expiresIn: expires_in_refresh_token
-    });
+    const token = this.tokenProvider.generateAccessToken(user.id!)
+    const refresh_token = this.tokenProvider.generateRefreshToken(
+      user.id!,
+      email
+    )
 
     const refresh_token_expires_date = this.dateProvider.addDays(
-      expires_refresh_token_days
-    );
+      authConfig.expires_refresh_token_days
+    )
 
     await this.usersTokenRepository.create({
       user_id: user.id,
       refresh_token,
-      expires_date: refresh_token_expires_date
-    });
+      expires_date: refresh_token_expires_date,
+    })
 
-    const tokenReturn: IResponse = {
+    return {
       token,
       user: {
         name: user.name,
         email: user.email,
-        avatar: getUserAvatarUrl(user, "avatar")
+        avatar: FileUrlBuilder.build(user.avatar, 'avatar'),
       },
-      refresh_token
-    };
-
-    return tokenReturn;
+      refresh_token,
+    }
   }
 }
